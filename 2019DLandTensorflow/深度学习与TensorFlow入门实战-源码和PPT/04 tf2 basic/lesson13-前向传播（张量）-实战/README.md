711de4a80a6f445907635277b86e2fd058eb7d59
jill_jiang <jill_jiang@wistron.com>
2022-07-05 20:27:46 +0800

    forward

---
+++ 2019DLandTensorflow/深度学习与TensorFlow入门实战-源码和PPT/04 tf2 basic/lesson13-前向传播（张量）-实战/README.md
@@ -0,0 +1,15 @@
+# tf2 basic
+
+## What we have learned
+* create tensor
+* indexing and slices
+* reshape and broadcasting
+* math operations
+
+
+![](recap.png)
+
+## 13 forward propagation  
+
+
+
--- 2019DLandTensorflow/深度学习与TensorFlow入门实战-源码和PPT/04 tf2 basic/lesson13-前向传播（张量）-实战/forward.py
+++ 2019DLandTensorflow/深度学习与TensorFlow入门实战-源码和PPT/04 tf2 basic/lesson13-前向传播（张量）-实战/forward.py
@@ -5,16 +5,15 @@ import  os
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-# x: [60k, 28, 28],
-# y: [60k]
+# x: [60k, 28, 28], y: [60k]
 (x, y), _ = datasets.mnist.load_data()
 # x: [0~255] => [0~1.]
 x = tf.convert_to_tensor(x, dtype=tf.float32) / 255.
 y = tf.convert_to_tensor(y, dtype=tf.int32)
 
-print(x.shape, y.shape, x.dtype, y.dtype)
-print(tf.reduce_min(x), tf.reduce_max(x))
-print(tf.reduce_min(y), tf.reduce_max(y))
+print(x.shape, y.shape, x.dtype, y.dtype)  #(60000, 28, 28) (60000,) <dtype: 'float32'> <dtype: 'int32'>
+print(tf.reduce_min(x), tf.reduce_max(x))  #[0.0, 1.0]
+print(tf.reduce_min(y), tf.reduce_max(y))  #[0, 9]
 
 
 train_db = tf.data.Dataset.from_tensor_slices((x,y)).batch(128)
